7323c443cfbe397058d0ff35fc0fbef4b533ab7b
mitch092 <stevenm7861@gmail.com>
2025-12-26 15:10:08 -0800

    Added new classes and ConvertUnits method.

---
+++ Chips/Chip8OpenGLRenderer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chips
+{
+    internal class Chip8OpenGLRenderer
+    {
+    }
+}
---
+++ Chips/DriverUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chips
+{
+    public static class DriverUtils
+    {
+        public static (long TargetUnits, long Remainder) ConvertUnits(
+            long targetUnitsPerBaseUnit, long sourceUnitsPerBaseUnit, long sourceUnits, long remainder)
+        {
+            long val = remainder + sourceUnits * targetUnitsPerBaseUnit;
+            long targetUnits = Math.DivRem(val, sourceUnitsPerBaseUnit, out long newRemainder);
+            return (targetUnits, newRemainder);
+        }
+    }
+}
